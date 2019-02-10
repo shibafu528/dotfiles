3b819d6da276984511d9b86fc58f63bbe390608d
shibafu <shibafu528@gmail.com>
2019-02-10 17:09:51 +0900

    Add docker-compose aliases

--- .cmdrc.bat
+++ .cmdrc.bat
@@ -15,3 +15,8 @@ doskey gch=git checkout $*
 doskey gd=git diff $*
 doskey gl=git log --decorate=full --all $*
 doskey gs=git status $*
+
+REM aliases / docker-compose command
+doskey dcon=docker-compose $*
+doskey dconu=docker-compose up -d $*
+doskey dcond=docker-compose down $*
