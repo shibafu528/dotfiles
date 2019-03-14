02a79a8d81f1c8cff6ce3186c4ff24f45e5428c1
shibafu <shibafu528@gmail.com>
2019-03-14 22:54:01 +0900

    update

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (helm-rg emmet-mode add-node-modules-path vue-mode prettier-js company-racer racer cargo flycheck-rust rust-mode helm-c-yasnippet yasnippet adoc-mode diminish open-junk-file avy yard-mode enh-ruby-mode projectile-ripgrep ripgrep jinja2-mode ddskk smart-cursor-color visual-regexp-steroids pcre2el which-key rainbow-delimiters helm-projectile projectile rspec-mode phpunit cmake-mode gitconfig-mode git-gutter-fringe quickrun inf-ruby ruby-electric yaml-mode sass-mode less-css-mode web-mode wgrep color-moccur undo-tree undohist helm powerline flycheck markdown-mode nlinum company editorconfig gitattributes-mode gitignore-mode php-mode powershell git-gutter magit dash))))
+    (docker-compose-mode dockerfile-mode helm-rg emmet-mode add-node-modules-path vue-mode prettier-js company-racer racer cargo flycheck-rust rust-mode helm-c-yasnippet yasnippet adoc-mode diminish open-junk-file avy yard-mode enh-ruby-mode projectile-ripgrep ripgrep jinja2-mode ddskk smart-cursor-color visual-regexp-steroids pcre2el which-key rainbow-delimiters helm-projectile projectile rspec-mode phpunit cmake-mode gitconfig-mode git-gutter-fringe quickrun inf-ruby ruby-electric yaml-mode sass-mode less-css-mode web-mode wgrep color-moccur undo-tree undohist helm powerline flycheck markdown-mode nlinum company editorconfig gitattributes-mode gitignore-mode php-mode powershell git-gutter magit dash))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
