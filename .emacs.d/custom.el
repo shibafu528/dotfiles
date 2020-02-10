8bf24e1f520346322c5cc0fca28d53ce58cab762
shibafu <shibafu528@gmail.com>
2020-02-10 10:44:02 +0900

    update

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -15,4 +15,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(markup-meta-face ((t (:stipple nil :foreground "dark gray" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "unknown" :family "Monospace"))))
+ '(markup-meta-hide-face ((t (:inherit markup-meta-face :foreground "gray60" :height 0.8))))
+ '(markup-title-0-face ((t (:inherit markup-gen-face :weight bold))))
+ '(markup-title-1-face ((t (:inherit markup-gen-face :weight bold))))
+ '(markup-title-2-face ((t (:inherit markup-gen-face :weight bold))))
+ '(markup-title-3-face ((t (:inherit markup-gen-face :weight bold))))
+ '(markup-title-4-face ((t (:inherit markup-gen-face :weight bold))))
+ '(markup-title-5-face ((t (:inherit markup-gen-face :weight bold)))))
