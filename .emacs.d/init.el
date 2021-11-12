65ea8b24e25152e6ffd7ce34df951e655e48d6be
shibafu <shibafu528@gmail.com>
2021-11-12 11:18:41 +0900

    cua-modeいらんかったわ（再放送）

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -60,8 +60,11 @@
 (recentf-mode t)
 
 ;; CUA mode
-(cua-mode t)
-(setq cua-enable-cua-keys nil)
+;; (cua-mode t)
+;; (setq cua-enable-cua-keys nil)
+
+;; リージョンをBackspaceで消せるようにする
+(delete-selection-mode t)
 
 ;; ivy
 (ivy-mode t)
