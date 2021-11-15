c6a79b396f30970835680fc7e8316bf5daae16e2
shibafu <shibafu528@gmail.com>
2021-11-15 19:13:52 +0900

    package-update-notify: pcaseで拘束する

--- .emacs.d/elisp/package-update-notify.el
+++ .emacs.d/elisp/package-update-notify.el
@@ -11,13 +11,13 @@
       (puthash (car pkg) (cadr pkg) archives))
     ;; インストール済のパッケージと比較
     (dolist (pkg package-alist)
-      (let* ((name (car pkg))
-             (installed (cadr pkg))
-             (upstream (gethash name archives)))
-        (when (and upstream
-                   (version-list-< (package-desc-version installed) (package-desc-version upstream)))
-          ;; アップデートがある
-          (add-to-list 'updates (cons name upstream) t))))
+      (pcase pkg
+        (`(,name ,installed)
+         (let ((upstream (gethash name archives)))
+           (when (and upstream
+                      (version-list-< (package-desc-version installed) (package-desc-version upstream)))
+             ;; アップデートがある
+             (add-to-list 'updates (cons name upstream) t))))))
     updates))
 
 (defun package-update-notify-callback ()
@@ -38,8 +38,8 @@
 (defun package-update-notify ()
   "パッケージの更新を確認し、ユーザーに通知する。"
   (interactive)
-    (add-hook 'package--post-download-archives-hook 'package-update-notify-callback)
-    (package-refresh-contents t))
+  (add-hook 'package--post-download-archives-hook 'package-update-notify-callback)
+  (package-refresh-contents t))
 
 ;; TODO: 毎回起動時に実行するのはアレなので、時々実行するような仕組みを作ってからフックかける
 ;; (add-hook 'emacs-startup-hook 'package-update-notify)
