70cd13192ab515a1dcc916ecff69477baf8ddb10
shibafu <shibafu528@gmail.com>
2021-11-15 22:52:23 +0900

    package-update-notify: 最終実行時刻の読み出しと実行判定の実装

--- .emacs.d/elisp/package-update-notify.el
+++ .emacs.d/elisp/package-update-notify.el
@@ -1,10 +1,17 @@
 ;;; package-update-notify --- パッケージの更新を確認し、ユーザーに通知する
 
 (require 'package)
-(require 'json)
 
 (defvar package-update-notify-cache (expand-file-name "package-update-notify" user-emacs-directory)
   "package-update-notifyの実行記録を付けるためのキャッシュファイルの保存先")
+
+(defun package-update-notify-read-cache ()
+  "package-update-notifyの実行記録を読み込む"
+  (when (file-exists-p package-update-notify-cache)
+    (with-temp-buffer
+      (insert-file-contents package-update-notify-cache)
+      (goto-char (point-min))
+      (read (current-buffer)))))
 
 (defun package-available-updates ()
   "更新のあるパッケージのリストを作成する。"
@@ -25,7 +32,7 @@
     updates))
 
 (defun package-update-notify-callback ()
-  "package-update-notify の非同期処理完了後のコールバック。"
+  "package-update-notifyの非同期処理完了後のコールバック。"
   (let ((updates (length (package-available-updates))))
     (when (<= 1 updates)
       (if (executable-find "notify-send")
@@ -39,19 +46,19 @@
         (message "更新可能なパッケージが %d 個あります" updates))))
   (remove-hook 'package--post-download-archives-hook 'package-update-notify-callback)
   (with-temp-file package-update-notify-cache
-    (insert (json-encode (current-time)))))
+    (prin1 (current-time) (current-buffer))))
 
 (defun package-update-notify ()
   "パッケージの更新を確認し、ユーザーに通知する。"
   (interactive)
-  ;; TODO: ファイルないときの処理
-  (let ((last-checked (append (json-read-file package-update-notify-cache) nil)))
-    (when (or (time-less-p (time-add last-checked (* 60 60 24 7)) (current-time))
-              (called-interactively-p 'any))
+  (let ((last-checked (package-update-notify-read-cache)))
+    (when (or (called-interactively-p 'any)
+              (not last-checked)
+              (time-less-p (time-add last-checked (* 60 60 24 7)) (current-time)))
       (add-hook 'package--post-download-archives-hook 'package-update-notify-callback)
       (package-refresh-contents t))))
 
 ;; TODO: 毎回起動時に実行するのはアレなので、時々実行するような仕組みを作ってからフックかける
-;; (add-hook 'emacs-startup-hook 'package-update-notify)
+(add-hook 'emacs-startup-hook 'package-update-notify)
 
 (provide 'package-update-notify)
