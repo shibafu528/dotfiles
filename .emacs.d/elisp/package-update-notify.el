cc1985f379fe59d5c441a69f86090604f10f79af
shibafu <shibafu528@gmail.com>
2021-11-15 23:19:47 +0900

    package-update-notify: bless

--- .emacs.d/elisp/package-update-notify.el
+++ .emacs.d/elisp/package-update-notify.el
@@ -1,5 +1,24 @@
 ;;; package-update-notify --- パッケージの更新を確認し、ユーザーに通知する
 
+;; Copyright (C) 2021  shibafu528
+
+;; Author: shibafu528
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a cop y of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Code:
+
 (require 'package)
 
 (defgroup package-update-notify nil "パッケージの更新通知の設定"
