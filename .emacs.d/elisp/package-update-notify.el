6ec8036697d636e9e1c10b36468c7f0c69cadb09
shibafu <shibafu528@gmail.com>
2021-11-15 20:05:19 +0900

    package-update-notify: 実行情報の保存

--- .emacs.d/elisp/package-update-notify.el
+++ .emacs.d/elisp/package-update-notify.el
@@ -1,6 +1,10 @@
 ;;; package-update-notify --- パッケージの更新を確認し、ユーザーに通知する
 
 (require 'package)
+(require 'json)
+
+(defvar package-update-notify-cache (expand-file-name "package-update-notify" user-emacs-directory)
+  "package-update-notifyの実行記録を付けるためのキャッシュファイルの保存先")
 
 (defun package-available-updates ()
   "更新のあるパッケージのリストを作成する。"
@@ -33,13 +37,19 @@
                          "-h" "string:desktop-entry:emacs"
                          (format "更新可能なパッケージが %d 個あります" updates))
         (message "更新可能なパッケージが %d 個あります" updates))))
-  (remove-hook 'package--post-download-archives-hook 'package-update-notify-callback))
+  (remove-hook 'package--post-download-archives-hook 'package-update-notify-callback)
+  (with-temp-file package-update-notify-cache
+    (insert (json-encode (current-time)))))
 
 (defun package-update-notify ()
   "パッケージの更新を確認し、ユーザーに通知する。"
   (interactive)
-  (add-hook 'package--post-download-archives-hook 'package-update-notify-callback)
-  (package-refresh-contents t))
+  ;; TODO: ファイルないときの処理
+  (let ((last-checked (append (json-read-file package-update-notify-cache) nil)))
+    (when (or (time-less-p (time-add last-checked (* 60 60 24 7)) (current-time))
+              (called-interactively-p 'any))
+      (add-hook 'package--post-download-archives-hook 'package-update-notify-callback)
+      (package-refresh-contents t))))
 
 ;; TODO: 毎回起動時に実行するのはアレなので、時々実行するような仕組みを作ってからフックかける
 ;; (add-hook 'emacs-startup-hook 'package-update-notify)
